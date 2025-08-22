e98decc1be67eb22bf3e73650c0bc4a6be5adb9d
Sanjana <sanjanatripathi709@gmail.com>
2025-08-22 18:49:26 +0530

    patterns

--- Patterns/01.cpp
+++ Patterns/01.cpp
@@ -34,6 +34,128 @@ void print2(int n){
     
 };
 
+void print3(int n){
+
+    /*  1 
+        1 2 
+        1 2 3        
+        1 2 3 4
+        1 2 3 4 5
+    */ 
+    for(int i = 1 ; i<=n; i++){
+        for (int j = 1; j <=i; j++){
+            cout << j <<" ";
+
+        };
+        cout<< endl;
+   }
+    
+};
+
+void print4(int n){
+
+    /*  1 
+        2 2 
+        3 3 3        
+        4 4 4 4
+        5 5 5 5 5
+    */ 
+    for(int i = 1 ; i<=n; i++){
+        for (int j = 1; j <=i; j++){
+            cout << i <<" ";
+
+        };
+        cout<< endl;
+   }
+    
+};
+
+void print5(int n){
+
+    /*  * * * * *
+        * * * *
+        * * * 
+        * * 
+        * 
+
+    */ 
+    for(int i = 1 ; i<=n; i++){
+        for (int j = 0 ; j < n-i+1; j++){
+            cout <<"* ";
+
+        };
+        cout<< endl;
+   }
+    
+};
+
+void print6(int n){
+
+    /*  1 2 3 4 5
+        1 2 3 4
+        1 2 3
+        1 2
+        1 
+
+    */ 
+    for(int i = 1 ; i<=n; i++){
+        for (int j = 1 ; j <= n-i+1; j++){
+            cout << j;
+
+        };
+        cout<< endl;
+   }
+    
+};
+
+
+void print7(int n){
+
+    /*         * 
+             * * * 
+           * * * * * 
+         * * * * * * * 
+       * * * * * * * * *
+    */ 
+   for(int i = 0 ; i<n; i++){
+    for(int j = 0; j < n-i-1; j++){
+            cout << " ";
+        }
+        for (int j = 0 ; j < 2*i+1; j++){
+            cout << "*";
+            }
+            for (int j = 0; j < n-i-1; j++){
+                cout << " "; 
+                }
+        cout<< endl;
+    }
+    
+};
+
+void print8(int n){
+
+    /*         
+       * * * * * * * * *
+         * * * * * * * 
+           * * * * *
+             * * * 
+               *      
+    */ 
+   for(int i = 0 ; i<n; i++){
+    for(int j = 0; j < i; j++){
+            cout << " ";
+        }
+        for (int j = 0 ; j < 2*n-(2*i+1); j++){
+            cout << "*";
+            }
+            for (int j = 0; j < i; j++){
+                cout << " "; 
+                }
+        cout<< endl;
+    }
+    
+};
+
 int main(){
     // for test cases
     /*int t;
@@ -45,6 +167,10 @@ int main(){
     }*/
     int n ;
         cin >> n;
-        print2(n);
+        /*pattern 9
+        print7(n);
+        print8(n);*/
+        print1(n);
+
 
 };
