9d0aca6747283b51bf24204015af76e86faf6c3a
Sanjana <sanjanatripathi709@gmail.com>
2025-08-23 00:50:41 +0530

    patterns

--- Patterns/02.cpp
+++ Patterns/02.cpp
@@ -266,10 +266,32 @@ void print21(int n){
 }
 }
 
+void print22(int n){
+    /*  
+       4444444
+       4333334
+       4322234
+       4321234
+       4322234
+       4333334
+       4444444
+    */
+   for(int i = 0; i<2*n-1; i++){
+        for(int j = 0; j<2*n-1; j++){
+            int top = i;
+            int left = j;
+            int right = (2*n-2)-j;
+            int bottom = (2*n-2)-i;
+            cout << n - min(min(top, left), min(right, bottom));
+        }
+        cout << endl;
+    }
+}
+
 
 int main() {
     int n;
     cin >> n; 
 
-    print21(n);
+    print22(n);
 }
