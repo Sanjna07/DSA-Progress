3d9dc7ec4d2cf70bb8f251ce4b889e83098bcb0f
Sanjana <sanjanatripathi709@gmail.com>
2025-09-09 01:09:49 +0530

    reverse_Arr

--- Basics/Recursion.cpp
+++ Basics/Recursion.cpp
@@ -53,6 +53,34 @@ int  add (int i ,int sum){
     add(i-1,sum+i);
 }
 
+//Functinal
+int add1(int n){
+    if (n==0) 
+    return 0;
+    return n + add1(n-1);
+}
+//factorial of n
+int fact(int n){
+    if (n==0) 
+    return 1;
+    return n * fact(n-1);
+}
+
+// reverse a array
+void Arr(int l , int r, int a[]){
+    if (l>=r) 
+    return;
+    swap(a[l],a[r]);
+    Arr(l+1,r-1,a);
+}
+
+void revArr(int i, int n, int a[]){
+    if (i>=n/2) 
+    return;
+    swap(a[i],a[n-i-1]);
+    revArr(i+1,n,a);
+}
+
 main(){
     //f();
     int n;
@@ -61,6 +89,23 @@ main(){
     //num(1,n);
     //rev(n,n);
     // numb(n,n);
-    add(n,0);
+    //add(n,0);
+    //cout << add1(n);
+    //cout << fact(n);
+
+    
+    // int a[n];
+    // for (int i=0;i<n;i++){
+    //     cin>>a[i];
+    // }
+    // //Arr(0,4,a);
+    // revArr(0,n,a);
+
+    // for (int i=0;i<n;i++){
+    //     cout<<a[i]<<" ";
+    // }
+   
+     
+
     return 0;
 }
