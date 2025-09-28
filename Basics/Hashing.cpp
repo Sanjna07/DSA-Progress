f72980dd7ca485f1c3501f8a65a470472d79e529
Sanjana <sanjanatripathi709@gmail.com>
2025-09-28 19:56:06 +0530

    char Hashing

--- Basics/Hashing.cpp
+++ Basics/Hashing.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+// int hashing 
+/* int main() {
     int n;
     cout << "Enter number of elements: ";
     cin >> n;
@@ -13,7 +14,7 @@ int main() {
     }
 
     // precomputation
-    int hash[13] = {0};
+    int hash[100] = {0};
     for (int i = 0; i < n; i++) {
         hash[arr[i]]++;
     }
@@ -29,5 +30,29 @@ int main() {
         cout << hash[num] << endl;
     }
 
+    return 0;
+}*/
+// string hashing
+int main() {
+    string s;
+    cout << "Enter the string: ";
+    cin>>s;
+    int q;
+    cout << "Enter number of queries: ";
+    cin>>q;
+
+    //pre compute
+    int hash[26]={0};
+    for (int i = 0; i< s.size(); i++){
+        hash[s[i]-'a']++;
+    }
+
+    while (q--){
+        char c;
+        cout<<"Enter character to query: ";
+        cin>>c;
+        //fetch 
+        cout<<hash[c-'a']<<endl;
+    }
     return 0;
 }
