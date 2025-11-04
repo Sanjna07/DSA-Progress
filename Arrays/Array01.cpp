492c37eb1c6ae8ab4b3cb35c624a5709ec4325fb
Sanjana <sanjanatripathi709@gmail.com>
2025-11-04 13:35:41 +0530

    Arrays

---
+++ Arrays/Array01.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// LargestElement
+
+int largestElement(int arr[], int n) {
+    int largest = arr[0];
+    for(int i = 1; i < n; i++) {
+        if(arr[i] > largest) {
+            largest = arr[i];
+        }
+    }
+    return largest;
+}
+
+int main() {
+    int n;
+    cout << "Enter the number of elements in the array: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter the elements of the array: ";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    int largest = largestElement(arr, n);
+    cout << "The largest element in the array is: " << largest << endl;
+    return 0;
+}
