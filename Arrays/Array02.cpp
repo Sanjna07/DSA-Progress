65a1cb1f5115e5a17591def163ed8dcd9d4f3720
Sanjana <sanjanatripathi709@gmail.com>
2025-11-10 23:51:27 +0530

    moving zeroes

--- Arrays/Array02.cpp
+++ Arrays/Array02.cpp
@@ -12,6 +12,49 @@ int leftRotateByOne(int arr[], int n) {
     return 0;
 }
 
+// left rotate array by d elements
+
+int leftRotateByD(int arr[], int n, int d) {
+    d = d % n; 
+
+    int temp[d];
+    for(int i = 0; i < d; i++) {
+         temp [i] = arr[i];
+    }
+
+    for(int i = d; i < n; i++) {
+        arr[i - d] = arr[i];
+    }
+
+    for(int i = n-d; i < n ; i++) {
+        arr[i] = temp[i-(n - d)];
+
+    }
+    return 0;
+}
+
+// Move Zeroes to end of array
+
+void moveZeroes(int arr[], int n) {
+    int j = -1;
+    for(int i = 0; i < n; i++) {
+        if(arr[i] == 0) {
+            j = i;
+            break;
+        }
+    }
+
+    if(j == -1) return ;
+
+    for(int i = j + 1; i < n; i++) {
+        if(arr[i] != 0) {
+            swap(arr[i], arr[j]);
+            j++;
+        }
+    }
+}
+
+
 int main() {
      int n;
     cout << "Enter the number of elements in the array: ";
@@ -22,8 +65,25 @@ int main() {
         cin >> a[i];
     }
 
-    leftRotateByOne(a, n);
+    // int d;
+    // cout << "Enter the number of positions to left rotate the array: ";
+    // cin >> d;
+
+    /*leftRotateByOne(a, n);
     cout << "Array after left rotation by one: ";
+    for(int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+     */
+
+    // leftRotateByD(a, n, d);
+    // cout << "\nArray after left rotation by "<< d <<": ";
+    // for(int i = 0; i < n; i++) {
+    //     cout << a[i] << " ";
+    // }
+
+    moveZeroes(a, n);
+    cout << "\nArray after moving zeroes to the end: ";
     for(int i = 0; i < n; i++) {
         cout << a[i] << " ";
     }
